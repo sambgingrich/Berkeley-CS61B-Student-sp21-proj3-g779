5514e5f00bb6411586ce25686ca95c66109d7736
Samuel Gingrich <sambgingrich@berkeley.edu>
2021-01-21 19:06:17 -0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,10 +1,20 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Samuel Gingrich
  */
 public class Collatz {
+    public static int nextNumber(int n){
+        if (n%2 == 0)
+            return n/2;
+        else
+            return n*3 + 1;
+    }
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
+        while (n !=1) {
+            System.out.print(n + " ");
+            n = nextNumber(n);
+        }
+       System.out.print("1");
 
     }
 }
