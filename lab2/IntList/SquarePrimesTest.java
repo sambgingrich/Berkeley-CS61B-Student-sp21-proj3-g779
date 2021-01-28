c4603cfe7694b1999d3f4475ee0b3a7346155026
Samuel Gingrich <sambgingrich@berkeley.edu>
2021-01-28 12:32:18 -0800

    Trying to push all of Lab 2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -16,5 +16,23 @@ public class SquarePrimesTest {
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
+
+    }
+
+    @Test
+    public void testSquarePrimes2() {
+        IntList lst = IntList.of(7, 8, 12, 5, 10);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("49 -> 8 -> 12 -> 25 -> 10", lst.toString());
+        assertTrue(changed);
+
+    }
+
+    @Test
+    public void testSquarePrimes3() {
+        IntList lst = IntList.of(23, 19, 25);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("529 -> 361 -> 25", lst.toString());
+        assertTrue(changed);
     }
 }
