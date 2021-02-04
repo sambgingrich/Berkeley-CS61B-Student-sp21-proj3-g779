602fa65e676b85f4e417a8c1c04a356d078736ef
Samuel Gingrich <sambgingrich@berkeley.edu>
2021-02-03 23:17:30 -0800

    Trying to get the Checkpoint Extra Credits

---
+++ proj1/deque/ArrayDequeTest.java
@@ -0,0 +1,86 @@
+package deque;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+public class ArrayDequeTest {
+
+    @Test
+    /** Adds a few things to the list, checking isEmpty() and size() are correct,
+     * finally printing the results.
+     *
+     * && is the "and" operation. */
+    public void addIsEmptySizeTest() {
+        ArrayDeque<String> ad1 = new ArrayDeque<String>();
+
+        assertTrue("A newly initialized aDeque should be empty", ad1.isEmpty());
+        ad1.addFirst("front");
+
+        // The && operator is the same as "and" in Python.
+        // It's a binary operator that returns true if both arguments true, and false otherwise.
+        assertEquals(1, ad1.size());
+        assertFalse("ad1 should now contain 1 item", ad1.isEmpty());
+
+        ad1.addLast("middle");
+        assertEquals(2, ad1.size());
+
+        ad1.addLast("back");
+        assertEquals(3, ad1.size());
+
+        System.out.println("Printing out deque: ");
+        ad1.printDeque();
+    }
+
+    @Test
+    /* Check if ArrayDeque is generic for all parameter types. */
+    public void multipleParamTest() {
+        ArrayDeque<String>  ad1 = new ArrayDeque<String>();
+        ArrayDeque<Double>  ad2 = new ArrayDeque<Double>();
+        ArrayDeque<Boolean> ad3 = new ArrayDeque<Boolean>();
+
+        ad1.addFirst("string");
+        ad2.addFirst(3.14159);
+        ad3.addFirst(true);
+
+        String s = ad1.removeFirst();
+        double d = ad2.removeFirst();
+        boolean b = ad3.removeFirst();
+    }
+
+    @Test
+    /* Tests removing from an empty deque */
+    public void removeEmptyTest() {
+        ArrayDeque<Integer> ad1 = new ArrayDeque<>();
+        ad1.addFirst(3);
+
+        ad1.removeLast();
+        ad1.removeFirst();
+        ad1.removeLast();
+        ad1.removeFirst();
+
+        int size = ad1.size();
+        String errorMsg = "  Bad size returned when removing from empty deque.\n";
+        errorMsg += "  student size() returned " + size + "\n";
+        errorMsg += "  actual size() returned 0\n";
+
+        assertEquals(errorMsg, 0, size);
+    }
+
+    @Test
+    /** Adds an item, then removes an item, and ensures that da is empty afterwards. */
+    public void addRemoveTest() {
+        ArrayDeque<Integer> ad1 = new ArrayDeque<Integer>();
+        // should be empty
+        assertTrue("ad1 should be empty upon initialization", ad1.isEmpty());
+
+        ad1.addFirst(10);
+        // should not be empty
+        assertFalse("ad1 should contain 1 item", ad1.isEmpty());
+
+        ad1.removeFirst();
+        // should be empty
+        assertTrue("ad1 should be empty after removal", ad1.isEmpty());
+    }
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,143 @@
+package deque;
+
+public class LinkedListDeque<T> {
+    private ThingNode sentinel;
+    private int size;
+
+
+    private class ThingNode {
+        public ThingNode previous;
+        public T item;
+        public ThingNode next;
+
+        public ThingNode(ThingNode p, T  i, ThingNode n) {
+            previous = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    public LinkedListDeque() {
+        sentinel = new ThingNode(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.previous = sentinel.next;
+        size = 0;
+    }
+
+    public LinkedListDeque(T item) {
+        sentinel = new ThingNode(null, null, null);
+        sentinel.next = new ThingNode(sentinel, item, sentinel);
+        sentinel.previous = sentinel.next;
+        size = 1;
+    }
+
+    /** Adds item of type T to the front of the deque. */
+    public void addFirst(T item){
+        sentinel.next = new ThingNode(sentinel, item, sentinel.next);
+        size += 1;
+    }
+
+    /** Adds item of type T to the back of the deque. */
+    public void addLast(T item){
+        sentinel.previous.next = new ThingNode(sentinel.previous, item , sentinel);
+        sentinel.previous = sentinel.previous.next;
+        size += 1;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    /** Prints the items in the deque from first to last, separated by a space.
+     * Once all the items have been printed, print out a new line. */
+    public void printDeque(){
+        ThingNode p  = sentinel;
+        while (p.next != sentinel){
+            p = p.next;
+            System.out.print(p.item + " ");
+        }
+        System.out.println();
+    }
+
+    /**Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null.*/
+    public T removeFirst(){
+        if (sentinel.next == sentinel) {
+            return null;
+        } else {
+            size -= 1;
+            T returnval = sentinel.next.item;
+            sentinel.next = sentinel.next.next;
+            sentinel.next.previous = sentinel;
+            return returnval;
+        }
+    }
+
+    /**Removes and returns the item at the back of the deque.
+     * If no such item exists, returns null */
+    public T removeLast(){
+        if (sentinel.next == sentinel) {
+            return null;
+        } else {
+            size -= 1;
+            T returnval = sentinel.previous.item;
+            sentinel.previous = sentinel.previous.previous;
+            sentinel.previous.next = sentinel;
+            return returnval;
+        }
+    }
+
+    /** Gets the item at that given index, where 0 is the front,
+     *  1 is the next item, and so forth. If no such item exists,
+     *  returns null. Does not alter the Deque.
+     */
+    public T get(int index){
+        int i =  0;
+        ThingNode p = sentinel.next;
+        while (i < index){
+            i += 1;
+            p = p.next;
+        }
+        return p.item;
+    }
+
+    /** Same as get, but recursive. */
+    public T getRecursive(int index){
+        System.out.println("Not sure about this one yet");
+        return null;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
