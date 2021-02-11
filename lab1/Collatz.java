b8f56fc198a1698f09c2f4f20d847d178e7e7de9
Samuel Gingrich <sambgingrich@berkeley.edu>
2021-02-11 12:51:47 -0800

    should have reverted back to the original solution of Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,26 +2,20 @@
  *  @author Samuel Gingrich
  */
 public class Collatz {
-
-    /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+    public static int nextNumber(int n){
+        if (n%2 == 0)
+            return n/2;
+        else
+            return n*3 + 1;
     }
-
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
-        while (n != 1) {
-            n = nextNumber(n);
+        while (n !=1) {
             System.out.print(n + " ");
+            n = nextNumber(n);
         }
-        System.out.println();
+       System.out.print("1");
+
     }
 }
 
