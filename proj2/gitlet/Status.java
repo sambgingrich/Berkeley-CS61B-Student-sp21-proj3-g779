9c78f6c390e53b609ddfad1c72069fbf4c574537
Samuel Gingrich <sambgingrich@berkeley.edu>
2021-03-27 10:14:57 -0700

    working on status still

--- proj2/gitlet/Status.java
+++ proj2/gitlet/Status.java
@@ -1,5 +1,10 @@
 package gitlet;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import static gitlet.Utils.*;
 import static gitlet.Repository.*;
 
 public class Status {
@@ -17,11 +22,22 @@ public class Status {
     }
 
     private static void branches() {
-        
+        List<String> branchList = plainFilenamesIn(BRANCHES_DIR);
+        String headUID = readContentsAsString(HEAD_FILE);
+        for (String branch : branchList) {
+            File branchFile = join(BRANCHES_DIR, branch);
+            if (readContentsAsString(branchFile).equals(headUID)) {
+                System.out.println("*" + branch);
+            } else {
+                System.out.println(branch);
+            }
+        }
     }
 
     private static void stagedFiles() {
-
+        for (Map.Entry<String, String> entry : addMap.entrySet()) {
+            System.out.println(entry.getKey());
+        }
     }
 
     private static void rmFiles() {
